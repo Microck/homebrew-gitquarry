fd549785bc3051452914f074605632e15524a018
Microck <contact@micr.dev>
2026-05-15 08:42:38 +0000

    chore: update gitquarry to v0.1.8

--- Formula/gitquarry.rb
+++ Formula/gitquarry.rb
@@ -1,30 +1,30 @@
 class Gitquarry < Formula
   desc "Terminal CLI for public GitHub repository search with explicit discovery controls"
   homepage "https://github.com/Microck/gitquarry"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Microck/gitquarry/releases/download/v0.1.7/gitquarry-v0.1.7-aarch64-apple-darwin.tar.gz"
-      sha256 "dd6f8531dd6eb97a07cbadf5d9c3dafe771ec799441e1f921046811d752b443a"
+      url "https://github.com/Microck/gitquarry/releases/download/v0.1.8/gitquarry-v0.1.8-aarch64-apple-darwin.tar.gz"
+      sha256 "7fa7b07e12eead5bcff4206c6e8506bc7920243990d6c82df20313c2c639f799"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/Microck/gitquarry/releases/download/v0.1.7/gitquarry-v0.1.7-x86_64-apple-darwin.tar.gz"
-      sha256 "5fbd5a219701baac0f821f1ae1642b59afeec6c8631409fd149363bd8e716e9c"
+      url "https://github.com/Microck/gitquarry/releases/download/v0.1.8/gitquarry-v0.1.8-x86_64-apple-darwin.tar.gz"
+      sha256 "675a2236123ec32a778aba95c471eb0832f9e099a66ff4dee596f3b6aabd42a6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Microck/gitquarry/releases/download/v0.1.7/gitquarry-v0.1.7-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0a20bac3bf6b3fc6c3119f5c570f11b5f4413bbc05646addff370f4fe0edb33d"
+      url "https://github.com/Microck/gitquarry/releases/download/v0.1.8/gitquarry-v0.1.8-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "aa17d2ce5f7861a38108528efab2d0c8ee2d642dc6b2b9205238fdf7f15e532d"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/Microck/gitquarry/releases/download/v0.1.7/gitquarry-v0.1.7-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ee0b7ed7e3c9367edbb8728168f613b4b3759573001e49eb35f23fbf46240197"
+      url "https://github.com/Microck/gitquarry/releases/download/v0.1.8/gitquarry-v0.1.8-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d8142f8dad82f1263c188baa1a4283bb1dd1fde8c617f8031e1b581edf8ae421"
     end
   end
 
